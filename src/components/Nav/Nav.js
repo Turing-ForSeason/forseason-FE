422d385044b4b36b87018345cfc8aace3dc58143
Yu-jimin <jiminyu0114@gmail.com>
2023-07-28 06:24:53 +0900

    feat: navbar 컴포넌트 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,1 +1,52 @@
-// Nav
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Nav,
+  NavLogo,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavElements';
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavLogo to="/" activeStyle>
+        FOR SEASON
+      </NavLogo>
+      <Bars />
+      <NavMenu>
+        <NavLink to="/weather" activeStyle>
+          날씨
+        </NavLink>
+        <NavLink to="/community/coordi" activeStyle>
+          코디 모아보기
+        </NavLink>
+        <NavLink to="/community/coordi/detail/:boardId" activeStyle>
+          코디 자세히 보기
+        </NavLink>
+        <NavLink to="/community/coordi/upload" activeStyle>
+          코디 업로드
+        </NavLink>
+        <NavLink to="/community/livetalk" activeStyle>
+          Live Talk
+        </NavLink>
+        <NavLink to="/mypage" activeStyle>
+          마이페이지
+        </NavLink>
+      </NavMenu>
+      <NavBtn>
+        <NavBtnLink to="/" activeStyle>
+          로그인
+        </NavBtnLink>
+        <NavBtnLink to="/" activeStyle>
+          마이페이지
+        </NavBtnLink>
+      </NavBtn>
+    </Nav>
+  );
+};
+
+export default Navbar;
--- src/components/Nav/NavElements.js
+++ src/components/Nav/NavElements.js
@@ -1,1 +1,98 @@
-// NavElement
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+
+export const Nav = styled.nav`
+  background: #ffffff;
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+`;
+
+export const NavLogo = styled(Link)`
+  color: #38a0ff;
+  font-family: 'Koulen', sans-serif;
+  font-size: 25px;
+  letter-spacing: 2%;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    color: #15cdfc;
+  }
+`;
+
+export const NavLink = styled(Link)`
+  color: #000000;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &:hover {
+    color: #38a0ff;
+    font-weight: bold;
+  }
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 20px;
+  background: #f2f2f2;
+  padding: 0.1rem 0.5rem;
+  margin-left: 10px;
+  color: #4e4e4e;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: #ffffff;
+    background: #52a1de;
+  }
+`;
