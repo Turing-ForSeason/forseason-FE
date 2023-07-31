fa922cd9946d7c22a3e6b56f9e22b6d13f08ea2c
Yeonjae Choi <fox2yj@naver.com>
2023-07-31 19:23:08 +0900

    chore: 마이페이지 관련 페이지.js 추가
    링크 연결 확인용 페이지들을 추가

---
+++ src/pages/MyBoardPage/MyBoardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Nav/Nav';
+
+const MyBoard = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>내 게시글</div>
+    </div>
+  );
+};
+
+export default MyBoard;
