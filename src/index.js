8ff1f09bf5c19525aa363e6ee15d47f39c8fc5ba
AnSui22 <ansui218@ewhain.net>
2023-07-31 17:24:31 +0900

    chore: skycons, bootstrap 설치 및 추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
