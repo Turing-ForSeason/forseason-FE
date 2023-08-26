11d6c6a44c481461ab44a59d67d8a50b96dbe8cf
AnSui22 <139625508+AnSui22@users.noreply.github.com>
2023-08-27 00:45:41 +0900

    chore: 커뮤니티 페이지 수정

--- src/pages/CommunityPage/CommunityPage.js
+++ src/pages/CommunityPage/CommunityPage.js
@@ -3,7 +3,6 @@ import Navbar from '../../components/Nav/Nav.js';
 import Footer from '../../components/Footer/Footer';
 import LiveTalkList from '../LiveTalkPage/LiveTalkList';
 import Coordi from './Coordi.js';
-import { Body } from '../LiveTalkPage/LiveTalkElements.js';
 
 function Community() {
   return (
