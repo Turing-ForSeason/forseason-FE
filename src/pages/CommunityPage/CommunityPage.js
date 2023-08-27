59a77bc6ea03139a594b00753686e96ab0696bd7
sui <ansui218@ewhain.net>
2023-08-28 03:02:16 +0900

    style: 커뮤니티 페이지 배경색 추가

--- src/pages/CommunityPage/CommunityPage.js
+++ src/pages/CommunityPage/CommunityPage.js
@@ -3,14 +3,17 @@ import Navbar from '../../components/Nav/Nav.js';
 import Footer from '../../components/Footer/Footer';
 import LiveTalkList from '../LiveTalkPage/LiveTalkList';
 import Coordi from './Coordi.js';
+import { Body } from '../LiveTalkPage/LiveTalkElements.js';
 
 function Community() {
   return (
     <div className="communityPage">
       <Navbar />
-      <LiveTalkList />
-      <Coordi />
-      <Footer />
+      <Body>
+        <LiveTalkList />
+        <Coordi />
+        <Footer />
+      </Body>
     </div>
   );
 }
