8ed52193a30f8ddaeb08f523d64f12a64c6176eb
sui <ansui218@ewhain.net>
2023-08-28 02:53:05 +0900

    chore: 커뮤니티 페이지 수정

--- src/pages/MainPage/BestCoordi.js
+++ src/pages/MainPage/BestCoordi.js
@@ -18,7 +18,7 @@ function BestCoordi() {
     navigate('/community');
   };
 
-  const [coordi, setCoordi] = useState([]);
+  const [coordiList, setCoordiList] = useState([]);
 
   const token = localStorage.getItem('Authorization');
 
@@ -33,45 +33,54 @@ function BestCoordi() {
         },
       )
       .then(res => {
-        const coordi = res.data;
-        setCoordi({
-          id: coordi.result.getContent[0].boardId,
-          img: coordi.result.getContent[0].boardPicture,
-          user: coordi.result.getContent[0].boardUserNickname,
-          like: coordi.result.getContent[0].boardLikeNum,
-          hashtag: coordi.result.getContent[0].boardHashtags,
-        });
+        const coordiData = res.data.result.getContent.map(coordi => ({
+          id: coordi.boardId,
+          img: coordi.boardPicture,
+          user: coordi.boardUserNickname,
+          like: coordi.boardLikeNum,
+          hashtag: coordi.boardHashtags,
+        }));
+        setCoordiList(coordiData);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
       });
   }, []);
 
   return (
     <ContentsContainer>
-      <Content1>
-        <h5>Contents</h5>
-        <h4>포시즌</h4>
-        <h4>실시간 BEST 3 코디</h4>
-        <h5>포시즌 유저가 추천하는 오늘의 코디를 확인해보세요.</h5>
+      {coordiList.length > 0 ? (
+        coordiList.map(coordi => (
+          <Content1 key={coordi.id}>
+            <h5>Contents</h5>
+            <h4>포시즌</h4>
+            <h4>실시간 BEST 3 코디</h4>
+            <h5>포시즌 유저가 추천하는 오늘의 코디를 확인해보세요.</h5>
 
-        <CoordinationContainer>
-          <img src={coordi.img} alt="코디사진" />
-          <Info>
-            <User>
-              <FontAwesomeIcon icon={faUser} />
-              <h4>{coordi.user}</h4>
-              <h4>님</h4>
-            </User>
-            <User>
-              <FontAwesomeIcon icon={faHeart} />
-              <h4>{coordi.like}</h4>
-            </User>
-            <User>
-              <span>{coordi.hashtag}</span>
-            </User>
-          </Info>
-        </CoordinationContainer>
+            <CoordinationContainer>
+              <img src={coordi.img} alt="코디사진" />
+              <Info>
+                <User>
+                  <FontAwesomeIcon icon={faUser} />
+                  <h4>{coordi.user}</h4>
+                  <h4>님</h4>
+                </User>
+                <User>
+                  <FontAwesomeIcon icon={faHeart} />
+                  <h4>{coordi.like}</h4>
+                </User>
+                <User>
+                  <span>{coordi.hashtag}</span>
+                </User>
+              </Info>
+            </CoordinationContainer>
 
-        <button onClick={navigateToCommunity}>커뮤니티 더보기 →</button>
-      </Content1>
+            <button onClick={navigateToCommunity}>커뮤니티 더보기 →</button>
+          </Content1>
+        ))
+      ) : (
+        <p>게시글이 없습니다</p>
+      )}
     </ContentsContainer>
   );
 }
